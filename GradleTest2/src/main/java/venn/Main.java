a676d880057eb78194485e8685c63ac616998e1c
brianmw1 <brianmw1@my.yorku.ca>
2020-01-22 15:11:03 -0500

    added prototype, modified build.gradle

--- GradleTest2/src/main/java/venn/Main.java
+++ GradleTest2/src/main/java/venn/Main.java
@@ -1,9 +1,56 @@
 package venn;
 
-public class Main {
+import javafx.application.Application;
+//import javafx.event.EventHandler;
+//import javafx.event.ActionEvent;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+//import javafx.scene.control.Button;
+import javafx.scene.shape.Circle;
+//import javafx.scene.layout.StackPane;
+import javafx.scene.Group;
 
-	public static void main (String [] args) {
-		System.out.println("Git test");
-		System.out.println("new line of code");
+import javafx.stage.Stage;
+
+
+public class Main extends Application{
+	
+	//Button button;
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception{
+		primaryStage.setTitle("Venn Diagram");
+	
+		Circle circle1 = new Circle();
+		circle1.setCenterX(250.0f);
+		circle1.setCenterY(135.0f);
+		circle1.setRadius(100.0f);
+		circle1.setStroke(Color.GREEN);
+		circle1.setFill(Color.rgb(200, 200, 200, 0.5));
+		
+		Circle circle2 = new Circle();
+		circle2.setCenterX(350.0f);
+		circle2.setCenterY(135.0f);
+		circle2.setRadius(100.0f);
+		circle2.setStroke(Color.BLUE);
+		circle2.setFill(Color.rgb(255, 0, 0, 0.2));
+		
+		//button =new Button();
+		//button.setText("click me");
+		//adds button to Venn Diagram Application 
+		
+		Group root = new Group();
+		
+		root.getChildren().add(circle1);
+		root.getChildren().add(circle2);
+		
+		Scene scene = new Scene(root, 600, 300);
+		primaryStage.setScene(scene);
+		primaryStage.show();
 	}
 }
+
